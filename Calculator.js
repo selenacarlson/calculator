4fab831a783426ce639daf9fad8e736ee44a7efb
Selena Carlson <selena.h.carlson@gmail.com>
2018-02-05 08:21:33 -0600

    rounded number

--- Calculator.js
+++ Calculator.js
@@ -30,6 +30,7 @@ $( document ).ready( function(){
 
 function displayEmployees(){
     var output = $( '#employeeOut' );
+    // output.empty();
     //loop through array and display on DOM
     for( i=0; i<employeeArray.length; i++ ){
       var outputString = '<li>';
@@ -37,10 +38,27 @@ function displayEmployees(){
       outputString += employeeArray[ i ].lastName + ' ';
       outputString += employeeArray[ i ].idNumber + ' ';
       outputString += employeeArray[ i ].jobTitle + ' ';
-      outputString += employeeArray[ i ].annualSalary;
+      outputString += '$' + employeeArray[ i ].annualSalary + ' ';
+      outputString += '<button id="removeButton" data-id="' + i + '">Remove</button></li>';
     }
       output.append( outputString );
   }; //end displayEmployees
+
+
+$( '#employeeOut' ).on( 'click', '#removeButton', function(){
+    var employeeID = $( this ).data( 'id' );
+    var removedEmployee = employeeArray.splice( employeeID, 1 );
+    // console.log( removedEmployee[ 0 ]);
+    // $( '#firstNameIn' ).val( removedEmployee[ 0 ].firstName );
+    // $( '#lastNameIn' ).val( removedEmployee[ 0 ].lastName );
+    // $( '#idNumberIn' ).val( removedEmployee[ 0 ].idNumber );
+    // $( '#jobTitleIn' ).val( removedEmployee[ 0 ].jobTitle );
+    // $( '#annualSalaryIn' ).val( removedEmployee[ 0 ].annualSalary );
+    //
+    // displayEmployees();
+
+
+  });
 
 function calculateCost(){
   var outputCost = $( 'h2' );
@@ -52,7 +70,7 @@ function calculateCost(){
   //add employee's monthly cost to total monthly cost
   monthlyCost += employeeMonthlyCost;
   //display monthly cost on DOM
-  outputCost.append( 'Monthly Cost: $' + monthlyCost );
+  outputCost.append( 'Monthly Cost: $' + Math.round(monthlyCost) );
 
 } //end calculateCost
 
